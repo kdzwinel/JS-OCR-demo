2aba050a5ee166776959032db21c352e981607c7
Kuo Chao Cheng <tony7199371@gmail.com>
2020-01-16 21:32:12 +0800

    Add micro server

---
+++ app.js
@@ -0,0 +1,25 @@
+// put this file in /dist/intergration
+/*  
+  -- do --
+
+  git init -y
+  npm i express
+  nodemon app.js
+
+  -- end --
+*/
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('./'));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname+'/index.html'))
+});
+
+app.listen(1764, function () {
+  console.log('Example app listening on port 1764!');
+});
+
